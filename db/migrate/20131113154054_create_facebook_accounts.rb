0b91519af4b08228756ef0362f8a793054ae24ed
Daniel Kimbel <danielkimbel@Daniels-MacBook-Pro.local>
2013-11-13 15:45:47 -0800

    Created models and migrations

--- db/migrate/20131113154054_create_facebook_accounts.rb
+++ db/migrate/20131113154054_create_facebook_accounts.rb
@@ -1,10 +1,10 @@
-class CreateUsers < ActiveRecord::Migration
+class CreateFacebookAccounts < ActiveRecord::Migration
   def change
-    create_table :users do |t|
+    create_table :facebook_accounts do |t|
+      t.belongs_to :user
       t.string :username
       t.string :oauth_token
       t.string :oauth_secret
-
       t.timestamps
     end
   end
