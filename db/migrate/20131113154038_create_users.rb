9db6a0387ed1dad6123aac57a5dd6340f8021d7f
Daniel Kimbel <danielkimbel@Daniels-MacBook-Pro.local>
2013-11-13 16:14:07 -0800

    Added account creation and login forms

--- db/migrate/20131113154038_create_users.rb
+++ db/migrate/20131113154038_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :email
       t.string :password
+      t.string :first_name
+      t.string :last_name
       t.timestamps
     end
   end
