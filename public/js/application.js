3968c47ea275f51b29817068e0fb8030e6569dd9
Daniel Kimbel <danielkimbel@Daniels-MacBook-Pro.local>
2013-11-15 00:16:32 -0800

    Cleaned up CSS with a new class

--- public/js/application.js
+++ public/js/application.js
@@ -10,7 +10,8 @@ $(document).ready(function() {
     setTimeout(displayScanning, 7000)
     setTimeout(function(){$("#scanning_message").toggleClass("bigger_text")}, 9000)    
     setTimeout(function(){$("#scanning_message").text("")}, 9000)
-    setTimeout(displayForm, 9000)
+    setTimeout(displayDeficiencies, 9000)
+    // setTimeout(displayForm, 11000)
   })
 
   function fadeOutEvaluate() {
@@ -25,6 +26,10 @@ $(document).ready(function() {
   function displayScanning() {
     $("#scanning_message").animate({opacity:1}, 500)
     setTimeout(function(){$("#scanning_message").animate({opacity:0}, 500)}, 1000)
+  }
+
+  function displayDeficiencies() {
+    $("#deficiencies").animate({opacity:1}, 500)
   }
 
   function displayForm() {
